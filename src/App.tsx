f0b73222101eb0fb8bfbbf2d5220b58ae661c9cc
kteneyck <kevin@teneyck.com>
2025-08-19 09:32:49 -0400

    move data to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ function App() {
     <div className="App">
       <TabView>
         <TabPanel header="Coenraet TenEyck">
-          <FamilyTree yamlPath="../../data/teneyck-tree.yaml" />
+          <FamilyTree yamlPath="/data/teneyck-tree.yaml" />
         </TabPanel>
         <TabPanel header="David TenEyck">
-          <FamilyTree yamlPath="../../data/teneyck-tree-later.yaml" />
+          <FamilyTree yamlPath="/data/teneyck-tree-later.yaml" />
         </TabPanel>
       </TabView>
     </div>
